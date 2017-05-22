e0c5bfe7f2c33dc3ac64cef1f98bd7b1b02b2c8c
ivan95603 <ivan95.603@hotmail.com>
2017-05-22 16:48:52 +0200

    a

---
+++ ProjectServer/Automobil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectServer
+{
+    enum status { nijeStigaoNaRed, popravljaSe, popravljen};
+
+    class Automobil
+    {
+        static int trenutniIdPopravke = 0;
+
+        int id_automobil;
+        string podaci;
+        status statusPopravke;
+        double cena_popravke;
+        string korisnicko_ime;
+        int id_popravke;
+
+
+    }
+}
---
+++ ProjectServer/Korisnik.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectServer
+{
+    public class Korisnik
+    {
+        string korisnicko_ime;
+        string lozinka;
+
+        public Korisnik(string korisnickoIme, string lozinka)
+        {
+            if (korisnickoIme == null) throw new ArgumentNullException(nameof(korisnickoIme));
+            if (lozinka == null) throw new ArgumentNullException(nameof(lozinka));
+            korisnicko_ime = korisnickoIme;
+            this.lozinka = lozinka;
+        }
+    }
+}
