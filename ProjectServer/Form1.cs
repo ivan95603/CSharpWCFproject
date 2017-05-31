cd6a3859c491b2b62f8d24ada394aed6208e6e9a
ivan95603 <ivan95.603@hotmail.com>
2017-06-01 00:03:12 +0200

    radi

--- ProjectServer/Form1.cs
+++ ProjectServer/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using ProjectLibrary;
+using System.Runtime.Serialization;
 
 namespace ProjectServer
 {
@@ -47,7 +48,6 @@ namespace ProjectServer
             InitializeComponent();
         }
 
-
         [ServiceContract(SessionMode = SessionMode.Required)]
         public interface IOperaterServis
         {
@@ -59,17 +59,20 @@ namespace ProjectServer
 
             [OperationContract]
             double SumaTroskovaNaAutuZaKorisnika();
+            /***
+             * Imam problem sa ovim mora da se podesi nesto za searializovanje liste kao [CollectionDataContract] samo sto je ovaj za klasu ili struct
+             * */
+            /*
+                       [OperationContract]
+                       List<status> StatusPopravkiNaAutu();
 
-            [OperationContract]
-            List<status> StatusPopravkiNaAutu();
-
-            [OperationContract]
-            List<Deo> PovuciDelove();
-
+                       [OperationContract]
+                       List<Deo> PovuciDelove();
+           */
 
         }
 
-
+        [CollectionDataContract]
         [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession, ConcurrencyMode = ConcurrencyMode.Single)]
         public class OperaterServis : IOperaterServis
         {
@@ -101,20 +104,27 @@ namespace ProjectServer
                 return 0;
             }
 
-            public List<status> StatusPopravkiNaAutu()
-            {
-                if (logovan)
-                {
-                    return Automobil.stanjaPopravkaNaKolima(korisnickoIme);
-                }
+            /***
+           * Imam problem sa ovim mora da se podesi nesto za searializovanje liste kao [CollectionDataContract] samo sto je ovaj za klasu ili struct
+           * */
+       
+            /*
+                        public List<status> StatusPopravkiNaAutu()
+                        {
+                            if (logovan)
+                            {
+                                return Automobil.stanjaPopravkaNaKolima(korisnickoIme);
+                            }
 
-                return null;
-            }
+                            return null;
+                        }
 
-            public List<Deo> PovuciDelove()
-            {
-                return ProjectLibrary.Deo.lagerDelova;
-            }
+                        public List<Deo> PovuciDelove()
+                        {
+                            return ProjectLibrary.Deo.lagerDelova;
+                        }
+
+             */
         }
 
 
@@ -122,94 +132,40 @@ namespace ProjectServer
 
 
 
-
-
-
-
-
-
-
-
-
-        /*
-        [ServiceContract]
-        public interface IJavniServis
-        {
-            [OperationContract]
-            double ProsecneTemperature(string stanica, DateTime pocetak, DateTime kraj);
-        }
-
-
-
-        public class JavniServis : IJavniServis
-        {
-            public double ProsecneTemperature(string stanica, DateTime pocetak, DateTime kraj)
-            {
-
-                return 0;
-            }
-        }
-
-    */
         private void Form1_Load(object sender, EventArgs e)
         {
             Init();
             Debug.WriteLine("IVANVNASJVALSKDJLKASJDLKSA");
 
-            // Create a WSHttpBinding and set its property values. 
-            WSHttpBinding binding = new WSHttpBinding();
-            binding.Name = "binding1";
-            binding.HostNameComparisonMode = HostNameComparisonMode.StrongWildcard;
-            binding.Security.Mode = SecurityMode.Message;
-            binding.ReliableSession.Enabled = true;
-            binding.TransactionFlow = false;
-            //Specify a base address for the service endpoint. 
-            Uri baseAddress = new Uri(@"http://localhost:8732/MeteoServis/OperaterServis");
-            // Create a ServiceHost for the CalculatorService type 
-            // and provide it with a base address. 
-            ServiceHost serviceHost = new ServiceHost(typeof(OperaterServis), baseAddress);
-            serviceHost.AddServiceEndpoint(typeof(IOperaterServis), binding, baseAddress);
-            // Open the ServiceHostBase to create listeners 
-            // and start listening for messages. 
+              // Create a WSHttpBinding and set its property values. 
+              WSHttpBinding binding = new WSHttpBinding();
+              binding.Name = "binding1";
+              binding.HostNameComparisonMode = HostNameComparisonMode.StrongWildcard;
+              binding.Security.Mode = SecurityMode.Message;
+              binding.ReliableSession.Enabled = true;
+              binding.TransactionFlow = false;
+              //Specify a base address for the service endpoint. 
+              Uri baseAddress = new Uri(@"http://localhost:8734/MeteoServis/OperaterServis");
+              // Create a ServiceHost for the CalculatorService type 
+              // and provide it with a base address. 
+              ServiceHost serviceHost = new ServiceHost(typeof(OperaterServis), baseAddress);
+              serviceHost.AddServiceEndpoint(typeof(IOperaterServis), binding, baseAddress);
+              // Open the ServiceHostBase to create listeners 
+              // and start listening for messages. 
 
-            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
-            smb.HttpGetEnabled = true;
-            smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
-            serviceHost.Description.Behaviors.Add(smb);
+              ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+              smb.HttpGetEnabled = true;
+              smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+              serviceHost.Description.Behaviors.Add(smb);
+
+              // Add MEX endpoint
+              serviceHost.AddServiceEndpoint(
+                  ServiceMetadataBehavior.MexContractName,
+                  MetadataExchangeBindings.CreateMexHttpBinding(),
+                  "mex"
+              );
 
             serviceHost.Open();
-
-
-
-
-
-
-
-
-
-
-
-            /*
-
-
-            Uri baseAddress2 = new Uri("http://localhost:8732/MeteoServis/JavniServis");
-
-
-        
-            // Create the ServiceHost.
-            ServiceHost host2 = new ServiceHost(typeof(JavniServis), baseAddress2);
-
-            // Enable metadata publishing.
-            ServiceMetadataBehavior smb2 = new ServiceMetadataBehavior();
-            smb2.HttpGetEnabled = true;
-            smb2.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
-            host2.Description.Behaviors.Add(smb2);
-
-            // Open the ServiceHost to start listening for messages. Since
-            // no endpoints are explicitly configured, the runtime will create
-            // one endpoint per base address for each service contract implemented
-            // by the service.
-            host2.Open();*/
 
         }
 
